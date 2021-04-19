7983c76cb71219a450dd0198e3fb5c65d7ba0d3b
JustBeta <jbarone@free.fr>
2021-04-19 16:40:04 +0200

    Add files via upload

--- privoxy/privoxy.sh
+++ privoxy/privoxy.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 
+apk add --no-cache privoxy 
+
+echo "confdir /etc/privoxy" > /etc/privoxy/config
+echo "" >> /etc/privoxy/config
+echo "listen-address  0.0.0.0:8118" >> /etc/privoxy/config
+echo "" >> /etc/privoxy/config
+echo "debug   1    # Show each GET/POST/CONNECT request" >> /etc/privoxy/config
+echo "debug   4096 # Startup banner and warnings" >> /etc/privoxy/config
