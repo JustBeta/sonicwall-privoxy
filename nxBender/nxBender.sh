9e381d2adfce8120b7d2b57ffb23e760c09f31c8
JustBeta <jbarone@free.fr>
2021-04-19 16:39:21 +0200

    Add files via upload

--- nxBender/nxBender.sh
+++ nxBender/nxBender.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
 
+apk --no-cache add ppp openssl
+
+# wget https://github.com/abrasive/nxBender/archive/refs/heads/master.zip -O /tmp/nxBender.zip
+wget https://github.com/JustBeta/nxBender/archive/refs/heads/master.zip -O /tmp/nxBender.zip
+unzip -o /tmp/nxBender.zip -d /
+cd /nxBender-master
+pip3 install .
+pip3 install -r requirements.txt
+cd /
+rm  /tmp/nxBender.zip
+rm -r /nxBender-master
